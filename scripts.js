ef2f9fb867248d73cccddcbdb64a0a498ed63483
Santeri Pigg <34033265+saneDG@users.noreply.github.com>
2019-11-16 11:45:31 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,54 @@
+
+// APIKEY 
+// 3b7720b960e90357d6bc883a5a765ab3
+
+// application ID
+// 0694a5e8
+
+// example req
+// https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}
+// &from=0&to=3&calories=591-722&health=alcohol-free
+
+rnd = Math.floor((Math.random() * 50) + 1);
+
+console.log(rnd)
+
+var url = "https://api.edamam.com/search?q=chicken&app_id=0694a5e8&app_key=3b7720b960e90357d6bc883a5a765ab3&from=" + rnd + "&to=" + (rnd + 1)
+
+$.ajax({
+    url: url,
+    dataType: 'json',
+    success: function (resp) {
+
+        var title = resp.hits[0].recipe.label
+        var imgUrl = resp.hits[0].recipe.image
+        var dietLabel = resp.hits[0].recipe.dietLabels[0]
+        var incredientLines = resp.hits[0].recipe.ingredientLines
+        var recipeUrl = resp.hits[0].recipe.url
+
+        console.log("title: " + title);
+        console.log("image url: " + imgUrl);
+        console.log("diet label: " + dietLabel);
+        console.log("recipe url: " + incredientLines);
+
+        $(".card-title").html(title)
+        $(".card-img-top").attr("src", imgUrl)
+        $(".card-subtitle").html(dietLabel)
+
+        function addListItem(index){
+            $( ".list-group" ).append("<li class=list-group-item>" + resp.hits[0].recipe.ingredientLines[index] + "</li>");
+        }
+
+        $.each(resp.hits[0].recipe.ingredientLines, function(index) {
+            addListItem(index)
+        });
+
+        $(".card-text").html(incredientLines)
+        $(".btn-recipe-link").attr("href", recipeUrl)
+        // No need to create a new jQuery object using $()
+        // result = $( end * newend );
+    },
+    error: function (req, status, err) {
+        console.log('Something went wrong', status, err);
+    }
+});
