047c848d8d8c7bc954ac8810467ab169f134aa5e
Santeri Pigg <santeri.pigg@edu.savonia.fi>
2019-11-16 12:37:14 +0200

    commit

--- scripts.js
+++ scripts.js
@@ -9,46 +9,55 @@
 // https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}
 // &from=0&to=3&calories=591-722&health=alcohol-free
 
-rnd = Math.floor((Math.random() * 50) + 1);
+$(".hide").hide()
 
-console.log(rnd)
+$(".reload").click(function(e){
+    e.preventDefault();
 
-var url = "https://api.edamam.com/search?q=chicken&app_id=0694a5e8&app_key=3b7720b960e90357d6bc883a5a765ab3&from=" + rnd + "&to=" + (rnd + 1)
+    var foodsearch = $(".food-search").val()
+    var rnd = Math.floor((Math.random() * 50) + 1);
+    var url = "https://api.edamam.com/search?q=" + foodsearch + "&app_id=0694a5e8&app_key=3b7720b960e90357d6bc883a5a765ab3&from=" + rnd + "&to=" + (rnd + 1)
 
-$.ajax({
-    url: url,
-    dataType: 'json',
-    success: function (resp) {
+    console.log(foodsearch)
 
-        var title = resp.hits[0].recipe.label
-        var imgUrl = resp.hits[0].recipe.image
-        var dietLabel = resp.hits[0].recipe.dietLabels[0]
-        var incredientLines = resp.hits[0].recipe.ingredientLines
-        var recipeUrl = resp.hits[0].recipe.url
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        success: function (resp) {
 
-        console.log("title: " + title);
-        console.log("image url: " + imgUrl);
-        console.log("diet label: " + dietLabel);
-        console.log("recipe url: " + incredientLines);
-
-        $(".card-title").html(title)
-        $(".card-img-top").attr("src", imgUrl)
-        $(".card-subtitle").html(dietLabel)
-
-        function addListItem(index){
-            $( ".list-group" ).append("<li class=list-group-item>" + resp.hits[0].recipe.ingredientLines[index] + "</li>");
+            $( ".list-group-item" ).remove();
+    
+            var title = resp.hits[0].recipe.label
+            var imgUrl = resp.hits[0].recipe.image
+            var dietLabel = resp.hits[0].recipe.dietLabels[0]
+            var incredientLines = resp.hits[0].recipe.ingredientLines
+            var recipeUrl = resp.hits[0].recipe.url
+    
+            console.log("title: " + title);
+            console.log("image url: " + imgUrl);
+            console.log("diet label: " + dietLabel);
+            console.log("recipe url: " + incredientLines);
+    
+            $(".card-title").html(title)
+            $(".card-img-top").attr("src", imgUrl)
+            $(".card-subtitle").html(dietLabel)
+    
+            function addListItem(index){
+                $( ".list-group" ).append("<li class=list-group-item> " + resp.hits[0].recipe.ingredientLines[index] + " </li>");
+            }
+    
+            $.each(resp.hits[0].recipe.ingredientLines, function(index) {
+                addListItem(index)
+            });
+    
+            $(".card-text").html(incredientLines)
+            $(".btn-recipe-link").attr("href", recipeUrl)
+            // No need to create a new jQuery object using $()
+            // result = $( end * newend );
+            $(".hide").show()
+        },
+        error: function (req, status, err) {
+            console.log('Something went wrong', status, err);
         }
-
-        $.each(resp.hits[0].recipe.ingredientLines, function(index) {
-            addListItem(index)
-        });
-
-        $(".card-text").html(incredientLines)
-        $(".btn-recipe-link").attr("href", recipeUrl)
-        // No need to create a new jQuery object using $()
-        // result = $( end * newend );
-    },
-    error: function (req, status, err) {
-        console.log('Something went wrong', status, err);
-    }
+    });
 });
